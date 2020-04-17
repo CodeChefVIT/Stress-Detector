6f0e07a01205a1c75df603de1a2cb86fb8d482e7
dhairyaostwal <dhairya.ostwal@gmail.com>
2020-04-17 09:43:54 +0530

    17.04.2020

---
+++ Keras files/1.py
@@ -0,0 +1,155 @@
+from __future__ import print_function
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+import os
+
+num_classes = 5 # 5 kinds of emotions 
+img_rows,Img_cols = 48,48 # target size = 48 x 48 images
+batch_size = 32# using 32 images at once for training
+
+# training and validation data 
+train_data_dir = '/Users/dhairyaostwal/Desktop/DO_Tech/train'
+validation_data_dir = '/Users/dhairyaostwal/Desktop/DO_Tech/validation'
+
+train_datagen = ImageDataGenerator(
+					rescale=1./255,
+					rotation_range=30, #rotating 30 degree
+					shear_range=0.3,
+					zoom_range=0.3,
+					width_shift_range=0.4,
+					height_shift_range=0.4,
+					horizontal_flip=True, # flipping image horizontally
+					fill_mode='nearest') # filling shifted pixels with the nearest
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+# training by converting image into grayscale which would give us structure and features of the face
+train_generator = train_datagen.flow_from_directory(
+					train_data_dir,
+					color_mode='grayscale',
+					target_size=(img_rows,img_cols),
+					batch_size=batch_size,
+					class_mode='categorical', # 5 classes of our emotions
+					shuffle=True) # shuffling -> better training
+
+validation_generator = validation_datagen.flow_from_directory(
+							validation_data_dir,
+							color_mode='grayscale',
+							target_size=(img_rows,img_cols),
+							batch_size=batch_size,
+							class_mode='categorical',
+							shuffle=True)
+
+# defining CNN(Convolutional Neural Network)
+model = Sequential()
+
+# Block-1
+
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu')) # using activation elu which has more +ve alphabet constants than relu which has -ve alphabet constants
+model.add(BatchNormalization())
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2)) # Dropout is  for preventing NN from Overfitting. Here randomly selected neurons are ignored during training
+
+# Similarly as we go on we keep on increasing batch_size*2
+# Block-2 
+
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-3
+
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-4 
+
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-5
+
+model.add(Flatten())
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5)) # Dropout = 50%
+
+# Block-6
+
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-7
+
+model.add(Dense(num_classes,kernel_initializer='he_normal'))
+model.add(Activation('softmax')) # Because we only want 5 kinds of emotions
+
+print(model.summary())
+
+from keras.optimizers import RMSprop,SGD,Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau # saving model, stopping if validation loss not improving
+
+checkpoint = ModelCheckpoint('Users/dhairyaostwal/Desktop/DO_Tech/Emotion_little_vgg.h5',
+                             monitor='val_loss',
+                             mode='min',
+                             save_best_only=True,
+                             verbose=1) 
+
+earlystop = EarlyStopping(monitor='val_loss',
+                          min_delta=0,
+                          patience=9,
+                          verbose=1,
+                          restore_best_weights=True
+                          )
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              patience=3,
+                              verbose=1,
+                              min_delta=0.0001)
+
+callbacks = [earlystop,checkpoint,reduce_lr]
+
+# compiling our model
+model.compile(loss='categorical_crossentropy',
+              optimizer = Adam(lr=0.001),
+              metrics=['accuracy'])
+
+nb_train_samples = 24176
+nb_validation_samples = 3006
+epochs=25
+
+history=model.fit_generator(
+                train_generator,
+                steps_per_epoch=nb_train_samples//batch_size,
+                epochs=epochs,
+                callbacks=callbacks,
+                validation_data=validation_generator,
+                validation_steps=nb_validation_samples//batch_size)
